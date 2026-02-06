689f866a92fa300830bf50eacc4322d6d260dc63
Harresh Varma a/l Ragunathan Naidu <resh@Harreshs-MacBook-Pro.local>
2026-02-06 10:39:56 +0800

    initial commit

---
+++ Lab 1 /Task 2.r
@@ -0,0 +1,8 @@
+num1 = 0.956786
+num2 = 7.8345901
+
+num1 = round(num1, digits = 3)
+num2 = round(num2, digits = 3)
+
+print(num1)
+print(num2)
---
+++ Lab 1 /Task 3.R
@@ -0,0 +1,4 @@
+radius <- readline(prompt = "Enter radius of Circle: ")
+radius <- as.numeric(radius)
+area <- 3.142 * radius * radius
+cat("The area of the circle is", area, "\n")
